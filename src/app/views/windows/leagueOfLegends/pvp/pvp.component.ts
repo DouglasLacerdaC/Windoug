1c39a6e5cfc4b629619f5db01239545fc1f8ff5f
Douglas <douglaslacerda544@gmail.com>
2022-04-20 21:26:09 -0300

    loading do launcher

--- src/app/views/windows/leagueOfLegends/pvp/pvp.component.ts
+++ src/app/views/windows/leagueOfLegends/pvp/pvp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pvp',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PvpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigate(router: string) {
+
+    this.router.navigate([router])
+
   }
 
 }
