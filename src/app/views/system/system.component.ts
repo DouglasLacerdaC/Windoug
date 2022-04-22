6ddb12321dca8166e1c20bad2c99f54e0f55aee7
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2022-04-22 17:44:58 -0300

    fix: funcao mover janela perde foco

--- src/app/views/system/system.component.ts
+++ src/app/views/system/system.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Launchers } from 'src/data/system/launchers';
-
+import { fromEvent, Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-system',
   templateUrl: './system.component.html',
   styleUrls: ['./system.component.scss']
 })
+
 export class SystemComponent implements OnInit {
 
   @ViewChild('window') window: any
@@ -18,63 +19,50 @@ export class SystemComponent implements OnInit {
   windowPositionY: any
   windowPositionX: any
 
-  constructor(private router: Router) { }
+  mouseMoveSubscription: Subscription
+  mouseUpSubscription: Subscription
+
+  constructor(private router: Router) {
+    this.mouseMoveSubscription = this.subscribeToMouseMove()
+    this.mouseUpSubscription = this.subscribeToMouseUp()
+  }
 
   ngOnInit(): void {
   }
 
+  private subscribeToMouseMove(): Subscription {
+    return fromEvent(document,'mousemove').subscribe((event) => {
+      if(this.pressed) {
+        this.window.nativeElement.style.top = `${(event as MouseEvent).clientY - this.windowPositionY}px`
+        this.window.nativeElement.style.left = `${(event as MouseEvent).clientX - this.windowPositionX}px`
+      }
+    })
+  }
+
+  private subscribeToMouseUp(): Subscription {
+    return fromEvent(document,'mouseup').subscribe(() => {
+      this.pressed = false;
+    })
+  }
+
   windowOpened(router: string) {
-    
     if(this.windowClass == '') {
-
       this.windowClass = 'opened'
-
       this.router.navigate([router])
-
     }
-
   }
 
   windowClosed() {
-    
     this.windowClass = ''
-  
+    this.pressed = false
     setTimeout(() => {
-
       this.router.navigate([''])
-
     }, 700)
-
   }
 
   headerPressed(event: MouseEvent) {
-    
     this.pressed = true
-    
     this.windowPositionY = event.offsetY
     this.windowPositionX = event.offsetX
-    
   }
-
-  headerMove(event: MouseEvent) {
-    
-    if(!this.pressed) {
-
-      return event.preventDefault()
-      
-    }
-    
-    this.window.nativeElement.style.top = `${event.clientY - this.windowPositionY}px`
-    this.window.nativeElement.style.left = `${event.clientX - this.windowPositionX}px`
-
-  }
-
-  headerUp() {
-
-    this.pressed = false
-
-  }
-
-  
-
 }
